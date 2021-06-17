c67a044fd6ce76fb20a68a2e73848975afd825c9
cdownie1 <cjdownie90@gmail.com>
2021-06-17 09:40:03 +0100

    minor updates for select

--- script.js
+++ script.js
@@ -14,71 +14,75 @@ const selectedChoice = document.querySelector('.questionChoice');
 /// INITIAL LOAD - MATERIALIZED CSS INITIALISERS.
 
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.questionChoice');
-    M.FormSelect.init(elems, {});
+  var elems = document.querySelectorAll('.questionChoice');
+  M.FormSelect.init(elems, {});
+});
+
+questionChoice.on('touchend', function (e) {
+  e.stopPropagation();
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.filterSelect');
-    M.FormSelect.init(elems, {});
+  var elems = document.querySelectorAll('.filterSelect');
+  M.FormSelect.init(elems, {});
 });
 
 selectedChoice.addEventListener('change', function (ev) {
-    category = ev.target.value;
-    localStorage.setItem('category', category);
-    startGameBtn.classList.remove('disabled');
+  category = ev.target.value;
+  localStorage.setItem('category', category);
+  startGameBtn.classList.remove('disabled');
 });
 
-
-
-
 filter.addEventListener('change', function (ev) {
-    category = ev.target.value;
-    // let rank = getHighScores ? 0 : 1;
+  category = ev.target.value;
+  // let rank = getHighScores ? 0 : 1;
 
-    if (category === 'all') {
-        retrieveScores(getHighScores);
-    } else {
-        const [...filteredScores] = getHighScores.filter(score => categories[score.category] === category);
-        retrieveScores(filteredScores);
-    }
+  if (category === 'all') {
+    retrieveScores(getHighScores);
+  } else {
+    const [...filteredScores] = getHighScores.filter(
+      (score) => categories[score.category] === category
+    );
+    retrieveScores(filteredScores);
+  }
 });
 
 const categories = {
-    9: 'General Knowledge',
-    21: 'Sport',
-    14: 'TV Shows',
-    11: 'Movies',
-    17: 'Science & Nature',
-    18: 'Computer Science'
-}
+  9: 'General Knowledge',
+  21: 'Sport',
+  14: 'TV Shows',
+  11: 'Movies',
+  17: 'Science & Nature',
+  18: 'Computer Science',
+};
 
 const retrieveScores = function (scores) {
-    scoreList.innerHTML = '';
-    lastScore = 101;
-    let rank = scores ? 0 : 1;
-    scores.forEach((score, index) => {
-        //retrieve prev score
-        !scores[(index - 1)] ? lastScore : lastScore = scores[(index - 1)].score;
-        //check if rank should be increased.
-        score.score < lastScore ? rank++ : rank;
-        //update UI
-        scoreList.innerHTML +=
-            `<tr>
+  scoreList.innerHTML = '';
+  lastScore = 101;
+  let rank = scores ? 0 : 1;
+  scores.forEach((score, index) => {
+    //retrieve prev score
+    !scores[index - 1] ? lastScore : (lastScore = scores[index - 1].score);
+    //check if rank should be increased.
+    score.score < lastScore ? rank++ : rank;
+    //update UI
+    scoreList.innerHTML += `<tr>
             <td>${categories[score.category]}</td>
             <td>${rank}</td>
             <td>${score.username}</td>
             <td>${score.score}</td> 
         </tr>`;
-    })
-}
+  });
+};
 
 //smooth scroll to high score section
-document.querySelector('.btn-highScore').addEventListener('click', function (ev) {
+document
+  .querySelector('.btn-highScore')
+  .addEventListener('click', function (ev) {
     ev.preventDefault();
     document.querySelector('#scores').scrollIntoView({
-        behavior: 'smooth'
+      behavior: 'smooth',
     });
-})
+  });
 
 retrieveScores(getHighScores);
